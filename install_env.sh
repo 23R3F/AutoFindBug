b02f9ef17cc77f341eec826ad1233587d1b0140a
23R3F <40877095+23R3F@users.noreply.github.com>
2020-11-04 16:13:43 +0800

    Update install_env.sh
    更新了安装脚本

--- install_env.sh
+++ install_env.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y python3-dev python3-pip libffi-dev build-essential virtualenvwrapper
 
-sudo apt-get install python3-dev libffi-dev build-essential virtualenvwrapper
-virtualenv --python=python3.5 angr_env
-source ./angr_env/bin/activate
+pip config set global.index-url 'https://mirrors.aliyun.com/pypi/simple'
+pip config set global.timeout '120'
+pip config set global.trusted-host 'mirrors.aliyun.com'
+
+python -m pip install update && python -m pip install --upgrade pip
+python3 -m pip install update && python3 -m pip install --upgrade pip
+
 pip install pwntools==4.0.1
+
+virtualenv --python=python3 angr_env
+source ./angr_env/bin/activate
+
 path=`pwd`
 echo "alias angr_env='source $path/angr_env/bin/activate'" > ~/.bashrc
 source ~/.bashrc
